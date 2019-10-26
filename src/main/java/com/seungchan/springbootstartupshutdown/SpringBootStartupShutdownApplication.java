b647911ae9e5753c84476a3ea1c1d3acce62b074
Seung-chan <mokdonjr@naver.com>
2019-10-26 21:19:11 +0900

    init spring project

---
+++ src/main/java/com/seungchan/springbootstartupshutdown/SpringBootStartupShutdownApplication.java
@@ -0,0 +1,13 @@
+package com.seungchan.springbootstartupshutdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartupShutdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStartupShutdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seungchan/springbootstartupshutdown/SpringBootStartupShutdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungchan.springbootstartupshutdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStartupShutdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
